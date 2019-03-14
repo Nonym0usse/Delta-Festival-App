a49e2a6c96ae2dc278efcf8051ff5cd215d8aa74
LOPEZ Doryan <doryan.lopez@ynov.com>
2019-03-14 09:48:28 +0100

    Ajout de la BDD

---
+++ Delta-Festival-Code/Database/Mapping/TeamMap.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Mapping
+{
+    class TeamMap : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.HasQueryFilter(b => EF.Property<int>(b, "MembersCount") <= 10);
+        }
+    }
+}
---
+++ Delta-Festival-Code/Database/Migrations/20190314074814_test-team-10Members.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class testteam10Members : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
